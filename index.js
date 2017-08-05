3cf91454eb408c87da31d91b8e1f3fbe12f29dd3
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-08-05 12:22:47 -0500

    reorganized views

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const client = new line.Client(config);
 // about Express itself: https://expressjs.com/
 const app = express();
 
-// setup view=
+// setup view
 app.get('/', function(request, response) {
   response.sendfile('./views/main.html');
 });
